8dc770187f12d2a1d39f6b3aece9015eb2186652
thinkam <1203948298@qq.com>
2017-12-17 13:39:42 +0800

    搭建项目框架，编写common模块

---
+++ rent-common/src/main/java/priv/thinkam/rent/common/annotation/BaseService.java
@@ -0,0 +1,13 @@
+package priv.thinkam.rent.common.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 初始化继承BaseService的/service
+ * Created by thinkam on 17-10-31.
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface BaseService {
+}
